29e254b3870dc1e73b4f08e212b956431fcdb43d
Johan Hanses <johanhanses@gmail.com>
2023-07-19 12:53:48 +0200

    Add prisma and connect to a db

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,3 +1,6 @@
+import { prisma } from '@/app/lib/database'
+import { User } from '@prisma/client'
+import bcrypt from 'bcrypt'
 import { NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
@@ -9,17 +12,62 @@ export const options: NextAuthOptions = {
         username: { label: 'Username', type: 'text', placeholder: 'username' },
         password: { label: 'Password', type: 'password', placeholder: 'password' },
       },
-      // @ts-ignore
       async authorize(credentials) {
-        const user = { id: 1, username: 'Johan', password: 'test' }
+        // const user = { id: 1, username: 'Johan', password: 'test' }
+        const user = await prisma.user.findUnique({
+          where: {
+            username: credentials?.username,
+          },
+        })
 
-        if (credentials?.username === user.username && credentials?.password === user.password) {
-          return user
+        // if (credentials?.username === user.username && credentials?.password === user.password) {
+        //   return user
+        // }
+
+        if (user && bcrypt.compareSync(credentials!.password, user.passwordHash)) {
+          return {
+            id: user.id,
+            username: user.username,
+          } as any
         }
         return null
       },
     }),
   ],
+
+  session: {
+    strategy: 'jwt',
+  },
+
+  callbacks: {
+    async jwt({ token, user }) {
+      let dbUser
+      if (token.user) {
+        dbUser = await prisma.user.findFirst({
+          where: {
+            username: (token.user as User).username,
+          },
+        })
+      }
+
+      if (!dbUser) {
+        token.user = user
+        return token
+      }
+
+      return {
+        name: dbUser.username,
+        email: null,
+        image: null,
+      }
+    },
+    session: ({ session, token }) => {
+      if (token && session) session.user = { name: token.name }
+
+      return session
+    },
+  },
+
   // pages: {
   //   signIn: '/auth/signin',
   // },
